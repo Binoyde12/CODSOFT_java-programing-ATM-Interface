fc9d169b1ccd58bd86bae32538ccc3e3fd8b1dd0
Binoy Krishna Debnath <128680894+Binoyde12@users.noreply.github.com>
2023-10-26 15:20:03 +0530

    added

---
+++ Account.java
@@ -0,0 +1,11 @@
+public class Account {
+	double accountBalance;
+
+	public double getAccountBalance() {
+		return accountBalance;
+	}
+
+	public void setAccountBalance(double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+}
